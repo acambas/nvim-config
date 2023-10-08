a4840d3844c7303839db9d7a6857a2fdfa63108c
Aleksandar Cambas <aleksandarcambas@gmail.com>
2023-10-08 21:34:50 +0200

    telescope file explorer

--- lua/acambas/plugins/telescope.lua
+++ lua/acambas/plugins/telescope.lua
@@ -1,28 +1,76 @@
 return {
-  'nvim-telescope/telescope.nvim',
-  tag = '0.1.3',
---    or                              , branch = '0.1.x',
-  dependencies = { 'nvim-lua/plenary.nvim' },
-  config = function()
-    pcall(require('telescope').load_extension, 'fzf')
+  {
+    'nvim-telescope/telescope.nvim',
+    tag = '0.1.3',
+    --    or                              , branch = '0.1.x',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    config = function()
+      pcall(require('telescope').load_extension, 'fzf')
+      require('telescope').setup {
+        defaults = {
+          mappings = {
+            i = {
+              ["<C-h>"] = "which_key"
+            }
+          }
+        },
+      }
+      vim.keymap.set('n', '<leader>sb', require('telescope.builtin').buffers, { desc = '[ ] Find existing buffers' })
+      vim.keymap.set('n', '<leader>/', function()
+        --      You can pass additional configuration to telescope to change theme, layout, etc.
+        require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
+          winblend = 10,
+          previewer = false,
+        })
+      end, { desc = '[/] Fuzzily search in current buffer' })
 
- --     See `:help telescope.builtin`
-    -- vim.keymap.set('n', '<leader><space>', require('telescope.builtin').oldfiles,
-    --   { desc = '[?] Find recently opened files' })
-    vim.keymap.set('n', '<leader>sb', require('telescope.builtin').buffers, { desc = '[ ] Find existing buffers' })
-    vim.keymap.set('n', '<leader>/', function()
-  --      You can pass additional configuration to telescope to change theme, layout, etc.
-      require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
-        winblend = 10,
-        previewer = false,
-      })
-    end, { desc = '[/] Fuzzily search in current buffer' })
+      vim.keymap.set('n', '<leader>p', require('telescope.builtin').git_files, { desc = 'Search [G]it [F]iles' })
+      vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
+      vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
+      vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
+      vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
+      vim.keymap.set('n', '<leader>sk', require('telescope.builtin').keymaps, { desc = '[S]earch [K]eymaps' })
+    end
+  },
+  {
+    "nvim-telescope/telescope-file-browser.nvim",
+    dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
+    config = function()
+      --
+      -- open file_browser with the path of the current buffer
+      --
+      --
+      require("telescope").setup {
+        extensions = {
+          file_browser = {
+            -- theme = "ivy",
+            -- disables netrw and use telescope-file-browser in its place
+            hijack_netrw = true,
+            mappings = {
+              ["i"] = {
+                -- your custom insert mode mappings
+              },
+              ["n"] = {
+                -- your custom normal mode mappings
+              },
+            },
+          },
+        },
+      }
 
-    vim.keymap.set('n', '<leader>gf', require('telescope.builtin').git_files, { desc = 'Search [G]it [F]iles' })
-    vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
-    vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
-    vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
-    vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
-    vim.keymap.set('n', '<leader>sk', require('telescope.builtin').keymaps, { desc = '[S]earch [K]eymaps' })
-  end
+      require("telescope").load_extension "file_browser"
+      vim.keymap.set(
+        "n",
+        "<space>e",
+        ":Telescope file_browser path=%:p:h select_buffer=true<CR>",
+        { noremap = true, desc = "open file explorer at curr buffer" }
+      )
+      vim.api.nvim_set_keymap(
+        "n",
+        "<space>E",
+        ":Telescope file_browser<CR>",
+        { noremap = true, desc = "open file explorer" }
+      )
+    end
+  }
 }

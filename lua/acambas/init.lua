6c4ce71c87b61d4336187d5f10c40c5f754ba795
Aleksandar Cambas <aleksandarcambas@gmail.com>
2023-10-14 23:06:20 +0200

    format and changed status line

--- lua/acambas/init.lua
+++ lua/acambas/init.lua
@@ -1,6 +1,9 @@
 require("acambas.keymaps")
 require("acambas.lazy")
 
+vim.opt.cmdheight = 0
+vim.opt.statusline = "%f - %y %=%S %l %L"
+vim.opt.showcmdloc = "statusline"
 -- Set highlight on search
 vim.o.hlsearch = false
 vim.opt.incsearch = true
@@ -11,7 +14,7 @@ vim.wo.relativenumber = true
 -- Sync clipboard between OS and Neovim.
 --  Remove this option if you want your OS clipboard to remain independent.
 --  See `:help 'clipboard'`
-vim.o.clipboard = 'unnamedplus'
+vim.o.clipboard = "unnamedplus"
 
 -- Enable break indent
 vim.o.breakindent = true
@@ -28,7 +31,7 @@ vim.o.updatetime = 250
 vim.o.timeoutlen = 300
 
 -- Set completeopt to have a better completion experience
-vim.o.completeopt = 'menuone,noselect'
+vim.o.completeopt = "menuone,noselect"
 
 vim.opt.wrap = false
 vim.opt.swapfile = false
@@ -45,11 +48,11 @@ vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 -- [[ Highlight on yank ]]
 -- See `:help vim.highlight.on_yank()`
-local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
-vim.api.nvim_create_autocmd('TextYankPost', {
-  callback = function()
-    vim.highlight.on_yank()
-  end,
-  group = highlight_group,
-  pattern = '*',
+local highlight_group = vim.api.nvim_create_augroup("YankHighlight", { clear = true })
+vim.api.nvim_create_autocmd("TextYankPost", {
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+	group = highlight_group,
+	pattern = "*",
 })

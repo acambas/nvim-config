e021dc0454872ebecb3adae3325dedb322a5c5bb
Aleksandar Cambas <aleksandarcambas@gmail.com>
2023-12-01 10:48:52 +0100

    fixes and updates

--- lua/acambas/plugins/theme.lua
+++ lua/acambas/plugins/theme.lua
@@ -8,12 +8,43 @@ end
 
 return {
 	{
+		"rebelot/kanagawa.nvim",
+		lazy = false,
+		priority = 1000,
+		-- enabled = false,
+		config = function()
+			require("kanagawa").setup({
+				compile = false, -- enable compiling the colorscheme
+				undercurl = true, -- enable undercurls
+				commentStyle = { italic = false },
+				functionStyle = {},
+				keywordStyle = { italic = false },
+				statementStyle = { bold = true },
+				typeStyle = {},
+				transparent = false, -- do not set background color
+				dimInactive = true, -- dim inactive window `:h hl-NormalNC`
+				terminalColors = true, -- define vim.g.terminal_color_{0,17}
+				colors = { -- add/modify theme and palette colors
+					palette = {},
+					theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
+				},
+				theme = "wave", -- Load "wave" theme when 'background' option is not set
+				background = { -- map the value of 'background' option to a theme
+					dark = "wave", -- try "dragon" !
+					light = "lotus",
+				},
+			})
+			-- vim.cmd("colorscheme kanagawa")
+		end,
+		cond = vscode_disable,
+	},
+	{
 		"rose-pine/neovim",
 		lazy = false,
 		priority = 1000,
 		-- enabled = false,
 		config = function()
-			require("rose-pine").setup({ disable_italics = true })
+			require("rose-pine").setup({ disable_italics = true, dim_nc_background = true })
 			vim.cmd("colorscheme rose-pine-main")
 		end,
 		cond = vscode_disable,
